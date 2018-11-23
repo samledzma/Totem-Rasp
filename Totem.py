40608ade39405b99919b49639d303f1cb5c4a764
Samuel Ledezma <samuel.ledzma@gmail.com>
2018-11-23 07:16:34 +0000

    Again

--- Totem.py
+++ Totem.py
@@ -12,6 +12,24 @@ import tarjetasUID
 reader = RFID.RFID()
 LCD_I2C.lcd_init()
 
+import serial
+
+ser = serial.Serial(
+    port='/dev/serial0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+           )
+
+def interprete(com):
+    com = com.split("|")
+    print com
+    if com[0] == "OPEN":
+     print "hola"
+
+
 def login():
  while True:
   print "inicio"
@@ -22,7 +40,6 @@ def login():
    id, text = reader.read()
   finally:
    GPIO.cleanup()
-# tarjeta = raw_input("SEND COMMAND: ")
   LCD_I2C.lcd_reset()
   LCD_I2C.lcd_print("   DETECTANDO   ",LCD_I2C.LCD_LINE_1) 
   LCD_I2C.lcd_print("     TARJETA    ",LCD_I2C.LCD_LINE_2)
@@ -46,10 +63,64 @@ def login():
    LCD_I2C.lcd_print("   POR FAVOR    ",LCD_I2C.LCD_LINE_2)
    time.sleep(3)
 
-threads = list()
-t1 = threading.Thread(target=login)
-threads.append(t1)
-t1.start()
-#t2 = threading.Thread(target=scanner_estaciones)
+def leer_estacion():
+ line = []
+
+ while True:
+    for c in ser.read():
+        line.append(c)
+        if c == '|':
+            print("Line: " + ''.join(line))
+            line = []
+            break
+
+ ser.close()
+
+
+
+
+
+
+
+
+
+
+#    try:
+#        escribir_estacion(adr,"REQ|"+str(adr))
+#        leer = 0
+#        texto=""
+#        while leer < 50:
+#            x = readNumber(hadr)
+#            if x == 10:
+#              break
+#            texto+=chr(x)
+#            leer+=1
+#        return texto
+#    except IOError as e:
+#        return "ERROR|CONNECTION_PROBLEM|"+str(adr);
+
+def escribir_estacion(adr,message):
+    print "Enviando mensaje --> "
+    print message
+    hadr=getHexAdr(adr)
+    try:
+        ser.write(message+'*')
+    except IOError as e:
+        #print e
+        return "ERROR|CONNECTION_PROBLEM|"+str(adr);
+
+
+#threads = list() 
+#t1 = threading.Thread(target=login)
+#threads.append(t1)
+#t1.start()
+ 
+#threading.Thread(target=scanner_estaciones)
 #threads.append(t2) t2.start()
 #reset_screen()
+
+while True:
+  print "Prueba"
+#  texto = raw_input("SEND COMMAND: ")
+#  ser.write(texto)
+  leer_estacion()
